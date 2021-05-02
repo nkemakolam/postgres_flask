17bbc882e653d176d1b27dc35196efba33570cea
Nkemakolam Nnadi <nkemakolamnnadi@nkemakolams-mbp.home>
2021-05-02 17:46:22 -0400

    creating database class so we can initialize database at will

--- connectingdb/project/app.py
+++ connectingdb/project/app.py
@@ -1,5 +1,8 @@
+from database import Database
 from user import User
-my_user = User ('moses@gmail.com','maxwell','josh',None)
+
+Database.initialise()
+my_user = User ('Raymond@gmail.com','maxwell','josh',None)
 print(my_user.save_to_db())
 
 my_user_data = User.load_from_db_by_email('Lizibert@gmail.com')
--- connectingdb/project/database.py
+++ connectingdb/project/database.py
@@ -1,12 +1,27 @@
 from psycopg2 import pool
-connection_pool = pool.SimpleConnectionPool(
-    1,
-    1,
-    user='nkemakolam',
-    password='ghosts123',
-    database='learning',
-    host='localhost'
-)
+
+class Database:
+    connection_pool = None
+    @classmethod
+    def initialise(cls):
+        cls.connection_pool = pool.SimpleConnectionPool(
+                                                        1,
+                                                        10,
+                                                        user='nkemakolam',
+                                                        password='ghosts123',
+                                                        database='learning',
+                                                        host='localhost'
+                                                    )
+
+    @classmethod
+    def get_connection(cls):
+        return cls.connection_pool.getconn()
+    @classmethod
+    def return_connection(cls, connection):
+        return cls.connection_pool.putconn(connection)
+    classmethod
+    def close_all_connection(cls):
+        Database.connection_pool.closeall()
 
 class CursorFromConnectFromPool:
     #create a connection 
@@ -16,7 +31,7 @@ class CursorFromConnectFromPool:
     
     #pick a connection from the pool
     def __enter__(self):
-        self.connection = connection_pool.getconn()
+        self.connection = Database.get_connection()
         self.cursor = self.connection.cursor()
         return self.cursor
     # exiting the connection to releae resource
@@ -26,4 +41,4 @@ class CursorFromConnectFromPool:
         else:
             self.cursor.close()
             self.connection.commit()
-        connection_pool.putconn(self.connection)
+        Database.return_connection(self.connection)
